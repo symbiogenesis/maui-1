2f0f15f013577efb474e948fc2b1e1b2a95c0020
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-07-28 13:34:05 +0200

    More tests

--- src/Controls/tests/DeviceTests/Elements/Label/LabelTests.iOS.cs
+++ src/Controls/tests/DeviceTests/Elements/Label/LabelTests.iOS.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Handlers;
 using UIKit;
+using Xunit;
 
 namespace Microsoft.Maui.DeviceTests
 {
 	public partial class LabelTests
 	{
+
+		[Fact(DisplayName = "Using TailTruncation LineBreakMode changes MaxLines")]
+		public async Task UsingTailTruncationSetMaxLines()
+		{
+			var label = new Label()
+			{
+				Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+				LineBreakMode = LineBreakMode.TailTruncation,
+			};
+
+			var handler = await CreateHandlerAsync<LabelHandler>(label);
+
+			var platformLabel = GetPlatformLabel(handler);
+
+			await InvokeOnMainThreadAsync((System.Action)(() =>
+			{
+				Assert.Equal(1, GetPlatformMaxLines(handler));
+				Assert.Equal(LineBreakMode.TailTruncation.ToPlatform(), GetPlatformLineBreakMode(handler));
+			}));
+		}
+				
 		UILabel GetPlatformLabel(LabelHandler labelHandler) =>
 			(UILabel)labelHandler.PlatformView;
 
