40561e9c1aa71804e53f4cb7d450e1736a3c292d
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-09-29 08:56:29 +0200

    Implement gestures on Windows Spans

--- src/Controls/src/Core/Label/Label.Windows.cs
+++ src/Controls/src/Core/Label/Label.Windows.cs
@@ -1,14 +1,44 @@
 #nullable disable
+using System.Collections.Generic;
 using Microsoft.Maui.Controls.Platform;
+using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.Maui.Controls
 {
 	public partial class Label
 	{
-		public static void MapDetectReadingOrderFromContent(LabelHandler handler, Label label) => MapDetectReadingOrderFromContent((ILabelHandler)handler, label);
-		public static void MapTextType(LabelHandler handler, Label label) => MapTextType((ILabelHandler)handler, label);
-		public static void MapText(LabelHandler handler, Label label) => MapText((ILabelHandler)handler, label);
+		TextBlock _textBlock;
 
+		private protected override void OnHandlerChangedCore()
+		{
+			base.OnHandlerChangedCore();
+
+			if (Handler is not null)
+			{
+				if (Handler is LabelHandler labelHandler && labelHandler.PlatformView is TextBlock textBlock)
+				{
+					_textBlock = textBlock;
+					_textBlock.SizeChanged += OnSizeChanged;
+				}
+			}
+			else
+			{
+				if (_textBlock is not null)
+				{
+					_textBlock.SizeChanged -= OnSizeChanged;
+					_textBlock = null;
+				}
+			}
+		}
+
+		public static void MapDetectReadingOrderFromContent(LabelHandler handler, Label label) =>
+			MapDetectReadingOrderFromContent((ILabelHandler)handler, label);
+
+		public static void MapTextType(LabelHandler handler, Label label) =>
+			MapTextType((ILabelHandler)handler, label);
+
+		public static void MapText(LabelHandler handler, Label label) =>
+			MapText((ILabelHandler)handler, label);
 
 		public static void MapDetectReadingOrderFromContent(ILabelHandler handler, Label label) =>
 			Platform.TextBlockExtensions.UpdateDetectReadingOrderFromContent(handler.PlatformView, label);
@@ -23,6 +53,65 @@ namespace Microsoft.Maui.Controls
 			handler.PlatformView?.UpdateLineBreakMode(label.LineBreakMode);
 
 		public static void MapMaxLines(ILabelHandler handler, Label label) =>
-			handler.PlatformView?.UpdateMaxLines(label);
+			handler.PlatformView?.UpdateMaxLines(label); 
+		
+		void OnSizeChanged(object sender, UI.Xaml.SizeChangedEventArgs e)
+		{
+			RecalculateSpanPositions();
+		}
+
+		void RecalculateSpanPositions()
+		{
+			if (Handler is LabelHandler labelHandler)
+			{
+				var platformView = labelHandler.PlatformView;
+				var virtualView = labelHandler.VirtualView as Label;
+
+				if (platformView is null || virtualView is null)
+					return;
+
+				IList<double> inlineHeights = GetInlineHeights();
+				platformView.RecalculateSpanPositions(virtualView, inlineHeights);
+			}
+		}
+
+		IList<double> GetInlineHeights()
+		{
+			IList<double> inlineHeights = new List<double>();
+
+			if (Handler is LabelHandler labelHandler)
+			{
+				var platformView = labelHandler.PlatformView;
+				var virtualView = labelHandler.VirtualView as Label;
+
+				if (virtualView is not null)
+				{
+					FormattedString formatted = virtualView.FormattedText;
+
+					if (formatted is not null)
+					{
+						var fontManager = virtualView.RequireFontManager();
+						platformView.Inlines.Clear();
+
+						// Have to implement a measure here, otherwise inline.ContentStart and ContentEnd will be null, when used in RecalculatePositions
+						platformView.Measure(new global::Windows.Foundation.Size(double.MaxValue, double.MaxValue));
+
+						var heights = new List<double>();
+						for (var i = 0; i < formatted.Spans.Count; i++)
+						{
+							var span = formatted.Spans[i];
+
+							var run = span.ToRunAndColorsTuple(fontManager).Item1;
+							heights.Add(platformView.FindDefaultLineHeight(run));
+							platformView.Inlines.Add(run);
+						}
+
+						inlineHeights = heights;
+					}
+				}
+			}
+
+			return inlineHeights;
+		}
 	}
 }
--- src/Controls/src/Core/Platform/Windows/Extensions/FormattedStringExtensions.cs
+++ src/Controls/src/Core/Platform/Windows/Extensions/FormattedStringExtensions.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Maui.Controls.Internals;
 using Microsoft.Maui.Graphics;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Documents;
+using WRect = Windows.Foundation.Rect;
 
 namespace Microsoft.Maui.Controls.Platform
 {
@@ -126,6 +129,57 @@ namespace Microsoft.Maui.Controls.Platform
 			run.CharacterSpacing = span.CharacterSpacing.ToEm();
 
 			return Tuple.Create(run, span.TextColor, span.BackgroundColor);
+		}
+
+		public static void RecalculateSpanPositions(this TextBlock control, Label element, IList<double> inlineHeights)
+		{
+			if (element?.FormattedText?.Spans == null
+				|| element.FormattedText.Spans.Count == 0)
+				return;
+
+			var labelWidth = control.ActualWidth;
+
+			if (labelWidth <= 0 || control.Height <= 0)
+				return;
+
+			for (int i = 0; i < element.FormattedText.Spans.Count; i++)
+			{
+				var span = element.FormattedText.Spans[i];
+
+				var inline = control.Inlines.ElementAt(i);
+
+				// TODO: Alternatives?. GetCharacterRect always return an empty Rect
+				var startRect = inline.ContentStart.GetCharacterRect(LogicalDirection.Forward);
+				var endRect = inline.ContentEnd.GetCharacterRect(LogicalDirection.Forward);
+
+				var defaultLineHeight = inlineHeights[i];
+				var yaxis = startRect.Top;
+
+				var lineHeights = new List<double>();
+
+				while (yaxis < endRect.Bottom)
+				{
+					double lineHeight;
+
+					if (yaxis == startRect.Top) // First Line
+					{
+						lineHeight = startRect.Bottom - startRect.Top;
+					}
+					else if (yaxis != endRect.Top) // Middle Line(s)
+					{
+						lineHeight = defaultLineHeight;
+					}
+					else // Bottom Line
+					{
+						lineHeight = endRect.Bottom - endRect.Top;
+					}
+
+					lineHeights.Add(lineHeight);
+					yaxis += lineHeight;
+				}
+
+				((ISpatialElement)span).Region = Region.FromLines(lineHeights.ToArray(), labelWidth, startRect.X, endRect.X + endRect.Width, startRect.Top).Inflate(10);
+			}
 		}
 	}
 }
