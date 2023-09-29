6f8e9efcee05464c4a0f72f1477d51d09157dc96
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-09-29 09:00:40 +0200

    Cleanup code

--- src/Controls/src/Core/Platform/Windows/Extensions/FormattedStringExtensions.cs
+++ src/Controls/src/Core/Platform/Windows/Extensions/FormattedStringExtensions.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Maui.Controls.Internals;
 using Microsoft.Maui.Graphics;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Documents;
-using WRect = Windows.Foundation.Rect;
 
 namespace Microsoft.Maui.Controls.Platform
 {
