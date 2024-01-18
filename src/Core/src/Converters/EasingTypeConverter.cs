65684bcf9c4144491b54248763791637b3c081f1
Edward Miller <symbiogenisis@outlook.com>
2024-01-18 13:07:57 -0600

    use dictionary for simpler and quicker lookups

--- src/Core/src/Converters/EasingTypeConverter.cs
+++ src/Core/src/Converters/EasingTypeConverter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
@@ -12,6 +13,23 @@ namespace Microsoft.Maui.Converters
 	/// <inheritdoc/>
 	public class EasingTypeConverter : TypeConverter
 	{
+		private static readonly Dictionary<string, Easing> StringToEasingMap = new(StringComparer.OrdinalIgnoreCase)
+		{
+			{ nameof(Linear), Linear },
+			{ nameof(SinIn), SinIn },
+			{ nameof(SinOut), SinOut },
+			{ nameof(SinInOut), SinInOut },
+			{ nameof(CubicIn), CubicIn },
+			{ nameof(CubicOut), CubicOut },
+			{ nameof(CubicInOut), CubicInOut },
+			{ nameof(BounceIn), BounceIn },
+			{ nameof(BounceOut), BounceOut },
+			{ nameof(SpringIn), SpringIn },
+			{ nameof(SpringOut), SpringOut }
+		};
+
+		private static readonly Dictionary<Easing, string> EasingToStringMap = StringToEasingMap.ToDictionary(pair => pair.Value, pair => pair.Key);
+
 		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
 			=> sourceType == typeof(string);
 
@@ -31,28 +49,8 @@ namespace Microsoft.Maui.Converters
 			if (parts.Length == 2 && parts[0] == nameof(Easing))
 				strValue = parts[parts.Length - 1];
 
-			if (strValue.Equals(nameof(Linear), StringComparison.OrdinalIgnoreCase))
-				return Linear;
-			if (strValue.Equals(nameof(SinIn), StringComparison.OrdinalIgnoreCase))
-				return SinIn;
-			if (strValue.Equals(nameof(SinOut), StringComparison.OrdinalIgnoreCase))
-				return SinOut;
-			if (strValue.Equals(nameof(SinInOut), StringComparison.OrdinalIgnoreCase))
-				return SinInOut;
-			if (strValue.Equals(nameof(CubicIn), StringComparison.OrdinalIgnoreCase))
-				return CubicIn;
-			if (strValue.Equals(nameof(CubicOut), StringComparison.OrdinalIgnoreCase))
-				return CubicOut;
-			if (strValue.Equals(nameof(CubicInOut), StringComparison.OrdinalIgnoreCase))
-				return CubicInOut;
-			if (strValue.Equals(nameof(BounceIn), StringComparison.OrdinalIgnoreCase))
-				return BounceIn;
-			if (strValue.Equals(nameof(BounceOut), StringComparison.OrdinalIgnoreCase))
-				return BounceOut;
-			if (strValue.Equals(nameof(SpringIn), StringComparison.OrdinalIgnoreCase))
-				return SpringIn;
-			if (strValue.Equals(nameof(SpringOut), StringComparison.OrdinalIgnoreCase))
-				return SpringOut;
+			if (StringToEasingMap.TryGetValue(strValue, out var easing))
+				return easing;
 
 			var fallbackValue = typeof(Easing)
 				.GetTypeInfo()
@@ -71,28 +69,8 @@ namespace Microsoft.Maui.Converters
 			if (value is not Easing easing)
 				throw new NotSupportedException();
 
-			if (easing == Linear)
-				return nameof(Linear);
-			if (easing == SinIn)
-				return nameof(SinIn);
-			if (easing == SinOut)
-				return nameof(SinOut);
-			if (easing == SinInOut)
-				return nameof(SinInOut);
-			if (easing == CubicIn)
-				return nameof(CubicIn);
-			if (easing == CubicOut)
-				return nameof(CubicOut);
-			if (easing == CubicInOut)
-				return nameof(CubicInOut);
-			if (easing == BounceIn)
-				return nameof(BounceIn);
-			if (easing == BounceOut)
-				return nameof(BounceOut);
-			if (easing == SpringIn)
-				return nameof(SpringIn);
-			if (easing == SpringOut)
-				return nameof(SpringOut);
+			if (EasingToStringMap.TryGetValue(easing, out var strValue))
+				return strValue;
 
 			throw new NotSupportedException();
 		}
@@ -104,18 +82,6 @@ namespace Microsoft.Maui.Converters
 			=> false;
 
 		public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
-			=> new(new[] {
-			"Linear",
-			"SinOut",
-			"SinIn",
-			"SinInOut",
-			"CubicIn",
-			"CubicOut",
-			"CubicInOut",
-			"BounceOut",
-			"BounceIn",
-			"SpringIn",
-			"SpringOut"
-			});
+			=> new(StringToEasingMap.Keys);
 	}
 }
